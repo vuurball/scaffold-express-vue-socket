fcb6dcf7b21280ffe7f9473baac9902bbf5e1f16
vuurball <none>
2020-01-12 17:53:58 +0200

    added bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
-  router,
-  store,
-  render: function(h) {
-    return h(App);
-  }
-}).$mount("#app");
+	router,
+	store,
+	render: function(h) {
+		return h(App);
+	},
+}).$mount('#app');
